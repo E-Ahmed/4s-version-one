7fc4335bb28d19eec145f1fc5da5dc811200c527
iamahmed <aelamry57@yahoo.com>
2018-11-28 10:33:37 +0200

    added new file at js folder to make counter is wright and edit at main.js file for counter

--- js/main.js
+++ js/main.js
@@ -184,20 +184,9 @@ function formatter(value, settings) {
 
 })(jQuery);
 
-jQuery(function ($) {
-  // custom formatting example
-  $('.count-number').data('countToOptions', {
-	formatter: function (value, options) {
-	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-	}
-  });
-
-  // start all the timers
-  $('.timer').each(count);
-
-  function count(options) {
-	var $this = $(this);
-	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-	$this.countTo(options);
-  }
-});
+jQuery(document).ready(function( $ ) {
+        $('.counter').counterUp({
+            delay: 20,
+            time: 5000
+        });
+    });

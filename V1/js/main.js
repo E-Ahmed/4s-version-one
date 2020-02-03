f99ebab4ef5b4e9420b31370414fc4bd654e0066
iamahmedelamry <aelamry57@yahoo.com>
2020-02-03 03:06:22 +0200

    add all of them

---
+++ V1/js/main.js
@@ -0,0 +1,202 @@
+(function($) {
+	//
+	// $("#btnclick, #btnclick1 , #btnclick2, #btnclick3, #btnclick4, #btnclick5").click(function() {
+	// 		$("#divpopup").dialog({
+	// 			  title:"ERP Solutions",
+	// 				width: 400,
+	// 				height: 200,
+	// 			  modal: true,
+	// 				buttons: {
+	//
+	// 				}
+	// 		});
+	// });
+
+	"use strict"
+///////////////////////////
+	// Preloader
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
+
+	///////////////////////////
+	// Scrollspy
+	$('body').scrollspy({
+		target: '#nav',
+		offset: $(window).height() / 2
+	});
+
+	///////////////////////////
+	// Smooth scroll
+	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+	$('#back-to-top').on('click', function(){
+		$('body,html').animate({
+			scrollTop: 0
+		}, 600);
+	});
+
+	///////////////////////////
+	// Btn nav collapse
+	$('#nav .nav-collapse').on('click', function() {
+		$('#nav').toggleClass('open');
+	});
+
+	///////////////////////////
+	// Mobile dropdown
+	$('.has-dropdown a').on('click', function() {
+		$(this).parent().toggleClass('open-drop');
+	});
+
+	///////////////////////////
+	// On Scroll
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+
+		// Fixed nav
+		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
+
+		// Back To Top Appear
+		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
+	});
+
+	///////////////////////////
+	// magnificPopup
+	$('.work').magnificPopup({
+		delegate: '.lightbox',
+		type: 'image'
+	});
+
+	///////////////////////////
+	// Owl Carousel
+	$('#about-slider').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animatein: 'fadein'
+	});
+	// $('#about-slider1').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:15,
+  //   dots : true,
+	// 	autoplay : true,
+	// 	animatein: 'fadein'
+	// });
+	$('#testimonial-slider').owlCarousel({
+		loop:true,
+		margin:15,
+		dots : true,
+		nav: false,
+		autoplay : true,
+		responsive:{
+			0: {
+				items:1
+			},
+			992:{
+				items:2
+			}
+		}
+	});
+
+	$.fn.countTo = function (options) {
+	options = options || {};
+
+	return $(this).each(function () {
+		// set options for current element
+		var settings = $.extend({}, $.fn.countTo.defaults, {
+			from:            $(this).data('from'),
+			to:              $(this).data('to'),
+			speed:           $(this).data('speed'),
+			refreshInterval: $(this).data('refresh-interval'),
+			decimals:        $(this).data('decimals')
+		}, options);
+
+		// how many times to update the value, and how much to increment the value on each update
+		var loops = Math.ceil(settings.speed / settings.refreshInterval),
+			increment = (settings.to - settings.from) / loops;
+
+		// references & variables that will change with each update
+		var self = this,
+			$self = $(this),
+			loopCount = 0,
+			value = settings.from,
+			data = $self.data('countTo') || {};
+
+		$self.data('countTo', data);
+
+		// if an existing interval can be found, clear it first
+		if (data.interval) {
+			clearInterval(data.interval);
+		}
+		data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+		// initialize the element with the starting value
+		render(value);
+
+		function updateTimer() {
+			value += increment;
+			loopCount++;
+
+			render(value);
+
+			if (typeof(settings.onUpdate) == 'function') {
+				settings.onUpdate.call(self, value);
+			}
+
+			if (loopCount >= loops) {
+				// remove the interval
+				$self.removeData('countTo');
+				clearInterval(data.interval);
+				value = settings.to;
+
+				if (typeof(settings.onComplete) == 'function') {
+					settings.onComplete.call(self, value);
+				}
+			}
+		}
+
+		function render(value) {
+			var formattedValue = settings.formatter.call(self, value, settings);
+			$self.html(formattedValue);
+		}
+	});
+};
+
+$.fn.countTo.defaults = {
+	from: 0,               // the number the element should start at
+	to: 0,                 // the number the element should end at
+	speed: 10,           // how long it should take to count between the target numbers
+	refreshInterval: 100,  // how often the element should be updated
+	decimals: 0,           // the number of decimal places to show
+	formatter: formatter,  // handler for formatting the value before rendering
+	onUpdate: null,        // callback method for every time the element is updated
+	onComplete: null       // callback method for when the element finishes updating
+};
+
+function formatter(value, settings) {
+	return value.toFixed(settings.decimals);
+}
+
+
+
+})(jQuery);
+
+//its for counter//
+
+jQuery(document).ready(function( $ ) {
+        $('.counter').counterUp({
+            delay: 20,
+            time: 5000
+        });
+    });
